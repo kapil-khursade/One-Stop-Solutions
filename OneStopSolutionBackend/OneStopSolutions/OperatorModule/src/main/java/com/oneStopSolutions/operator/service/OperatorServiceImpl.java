861fb43017d8c33ca0a54e5328cfc4b0fa6606e1
SubhadeepBid <subhadipbid@gmail.com>
2023-01-21 18:25:25 +0530

    Updates

--- OneStopSolutionBackend/OneStopSolutions/OperatorModule/src/main/java/com/oneStopSolutions/operator/service/OperatorServiceImpl.java
+++ OneStopSolutionBackend/OneStopSolutions/OperatorModule/src/main/java/com/oneStopSolutions/operator/service/OperatorServiceImpl.java
@@ -6,6 +6,8 @@ import java.util.List;
 import java.util.Optional;
 import java.util.Set;
 
+import javax.persistence.Transient;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
