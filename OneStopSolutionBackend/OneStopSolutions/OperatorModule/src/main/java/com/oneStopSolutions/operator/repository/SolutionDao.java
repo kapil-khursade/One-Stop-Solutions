128e764982f1e64f19704c50543e5bd14c080d11
Mukta Raj <mukta.raj0@gmail.com>
2023-01-21 23:05:52 +0530

    operatorServive updated

--- OneStopSolutionBackend/OneStopSolutions/OperatorModule/src/main/java/com/oneStopSolutions/operator/repository/SolutionDao.java
+++ OneStopSolutionBackend/OneStopSolutions/OperatorModule/src/main/java/com/oneStopSolutions/operator/repository/SolutionDao.java
@@ -1,11 +1,15 @@
 package com.oneStopSolutions.operator.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import com.oneStopSolutions.customer.customerBeans.Issue;
 import com.oneStopSolutions.operator.Beans.Solution;
 
 @Repository
 public interface SolutionDao extends JpaRepository<Solution, Integer> {
 
+	public List<Solution> findByIssue(Issue issue);
 }
