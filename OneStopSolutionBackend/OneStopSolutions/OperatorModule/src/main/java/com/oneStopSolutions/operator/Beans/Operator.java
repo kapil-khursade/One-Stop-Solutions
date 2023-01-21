35774f0b92fec0dfa2a8f497e718f48cdb3cbab3
Mukta Raj <mukta.raj0@gmail.com>
2023-01-21 16:45:35 +0530

    removed webConfig

--- OneStopSolutionBackend/OneStopSolutions/OperatorModule/src/main/java/com/oneStopSolutions/operator/Beans/Operator.java
+++ OneStopSolutionBackend/OneStopSolutions/OperatorModule/src/main/java/com/oneStopSolutions/operator/Beans/Operator.java
@@ -43,6 +43,8 @@ public class Operator {
 	@OneToOne(cascade = CascadeType.ALL)
 	private Login login;
 	
+	
+	
 	@OneToMany(cascade = CascadeType.ALL, mappedBy = "operator")
 	private Set<Solution> solutions=new HashSet<Solution>();
 }
